f10a4c4fe7527183b624ea63e8654aa0a6839b8a
Rodrik Vartoomian <55018126+hottunasandwich@users.noreply.github.com>
2021-03-26 09:51:45 +0430

    deleted comments

--- math.py
+++ math.py
@@ -1,25 +1,15 @@
-# importing web driver
 import time
 from selenium import webdriver
-# to do some chained actions for non-button options
 from selenium.webdriver.common.action_chains import ActionChains
-# inteligent waiting options
 from os import path
 from selenium.webdriver.support.ui import WebDriverWait
-# to put some conditions on page to perform an action
 from selenium.webdriver.support import expected_conditions as EC
-# methods of gathering element
-# in this element we used css selector but i dont think it was needed :)))))
 from selenium.webdriver.common.by import By
-# measure the time we wasted or saved i dont really know !
 
 driver = webdriver.Chrome(path.join(path.dirname(__file__),'chromedriver.exe'))
-# the end range of for below has to be POW(2,15) cause we have this much possiblities
 start_time = time.time()
 for j in range(0, 1001):
-    # driver get the page of adobe creative
     driver.get("https://tbot.xyz/math/#eyJ1IjoyMDk3MDI4NjAsIm4iOiJSb2RyaWsgIiwiZyI6Ik1hdGhCYXR0bGUiLCJjaSI6Ii04MjA3MTU5MDEwMjQ2NzIyMjk3IiwiaSI6IkJBQUFBQTVmQXdETXozOE1ueEtTRlA2VU9SYyJ9ODM2NjEwYThkZmEyZTY1ZjA5ZmE5ZjljMDc1MDM4YmI=&tgShareScoreUrl=tg%3A%2F%2Fshare_game_score%3Fhash%3Du5Rj08qAKPxqHUq0_uBMbueWind7mWtjNPZKubwHgtU")
-    # wait for test button to appear
 
     if (j == 0):
         element = WebDriverWait(driver, 10).until(
@@ -69,10 +59,8 @@ for j in range(0, 1001):
     
 
 
-    # click the element
     element.click()
 time.sleep(5)
-# print the time that took to perform this action
 print("-------------")
 print(time.time() - start_time)
 print("-------------")
